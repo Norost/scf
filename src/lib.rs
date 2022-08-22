d11c8e5d789058bfe6ea1bb9a9dc05862723039e
David Hoppenbrouwers <david@salt-inc.org>
2022-08-22 02:15:50 +0200

    Add groups iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 
-use core::str;
+use core::{cell::Cell, str};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Token<'a> {
@@ -21,13 +21,6 @@ impl<'a> Token<'a> {
 pub struct Iter<'a> {
 	data: &'a [u8],
 	index: usize,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum Error {
-	UnterminatedQuote,
-	InvalidSymbolChar,
-	InvalidUtf8,
 }
 
 impl<'a> Iterator for Iter<'a> {
@@ -84,8 +77,133 @@ impl<'a> Iterator for Iter<'a> {
 	}
 }
 
+#[derive(Debug)]
+#[must_use = "an error may have occured"]
+pub struct Groups<'a> {
+	data: &'a [u8],
+	index: Cell<usize>,
+}
+
+impl<'a> Groups<'a> {
+	pub fn iter(&mut self) -> GroupsIter<'a, '_> {
+		GroupsIter { inner: Some(self) }
+	}
+
+	pub fn into_error(self) -> Option<Error> {
+		Error::from_num(self.index.get())
+	}
+}
+
+#[derive(Debug)]
+pub struct GroupsIter<'a, 'b> {
+	inner: Option<&'b Groups<'a>>,
+}
+
+impl<'a, 'b> GroupsIter<'a, 'b> {
+	pub fn next_str(&mut self) -> Option<&'a str> {
+		self.next().and_then(|e| e.into_str())
+	}
+
+	pub fn next_group(&mut self) -> Option<GroupsIter<'a, 'b>> {
+		self.next().and_then(|e| e.into_group())
+	}
+}
+
+impl<'a, 'b> Iterator for GroupsIter<'a, 'b> {
+	type Item = Item<'a, 'b>;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let r = self.inner?;
+		let mut it = Iter {
+			data: r.data,
+			index: r.index.get(),
+		};
+		if (it.index as isize) < 0 {
+			return None;
+		}
+		let tk = it.next();
+		r.index.set(it.index);
+		match tk {
+			None => None,
+			Some(Err(e)) => {
+				r.index.set(e.into_num());
+				None
+			}
+			Some(Ok(tk)) => Some(match tk {
+				Token::Str(s) => Item::Str(s),
+				Token::Begin => Item::Group(Self { inner: self.inner }),
+				Token::End => {
+					self.inner = None;
+					return None
+				}
+			}),
+		}
+	}
+}
+
+impl Drop for GroupsIter<'_, '_> {
+	fn drop(&mut self) {
+		for _ in self {}
+	}
+}
+
+impl core::iter::FusedIterator for GroupsIter<'_, '_> {}
+
+#[derive(Debug)]
+pub enum Item<'a, 'b> {
+	Str(&'a str),
+	Group(GroupsIter<'a, 'b>),
+}
+
+impl<'a, 'b> Item<'a, 'b> {
+	pub fn into_str(self) -> Option<&'a str> {
+		match self {
+			Self::Str(s) => Some(s),
+			_ => None,
+		}
+	}
+
+	pub fn into_group(self) -> Option<GroupsIter<'a, 'b>> {
+		match self {
+			Self::Group(g) => Some(g),
+			_ => None,
+		}
+	}
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+	UnterminatedQuote,
+	InvalidSymbolChar,
+	InvalidUtf8,
+}
+
+impl Error {
+	fn into_num(self) -> usize {
+		(match self {
+			Self::UnterminatedQuote => -1,
+			Self::InvalidSymbolChar => -2,
+			Self::InvalidUtf8 => -3,
+		}) as usize
+	}
+
+	fn from_num(n: usize) -> Option<Self> {
+		Some(match n as isize {
+			-1 => Self::UnterminatedQuote,
+			-2 => Self::InvalidSymbolChar,
+			-3 => Self::InvalidUtf8,
+			_ => return None,
+		})
+	}
+}
+
+#[deprecated(note = "use `parse2`, which is less error-prone")]
 pub fn parse<'a>(data: &'a [u8]) -> Iter<'a> {
 	Iter { data, index: 0 }
+}
+
+pub fn parse2<'a>(data: &'a [u8]) -> Groups<'a> {
+	Groups { data, index: 0.into() }
 }
 
 #[cfg(test)]
@@ -101,6 +219,7 @@ mod test {
 		(1050 "drivers/pci/virtio/gpu"))
 	(8086 ; Intel
 		(1616 "drivers/pci/intel/hd graphics"))) ; intentional space"#;
+		#[allow(deprecated)]
 		let mut it = parse(t);
 		assert_eq!(it.next(), Some(Ok(Token::Begin)));
 		assert_eq!(it.next(), Some(Ok(Token::Str("pci-drivers"))));
@@ -131,5 +250,116 @@ mod test {
 		assert_eq!(it.next(), Some(Ok(Token::End)));
 		assert_eq!(it.next(), Some(Ok(Token::End)));
 		assert_eq!(it.next(), None);
+	}
+
+	#[test]
+	fn example_pci_groups() {
+		let t = br#"(pci-drivers
+	(1af4 ; Red Hat
+		(1000 "drivers/pci/virtio/net")
+		(1001 "drivers/pci/virtio/blk")
+		(1050 "drivers/pci/virtio/gpu"))
+	(8086 ; Intel
+		(1616 "drivers/pci/intel/hd graphics"))) ; intentional space"#;
+		#[track_caller]
+		fn string<'a, 'b>(it: &mut GroupsIter<'a, 'b>) -> &'a str {
+			it.next().unwrap().into_str().unwrap()
+		}
+		#[track_caller]
+		fn group<'a, 'b>(it: &mut GroupsIter<'a, 'b>) -> GroupsIter<'a, 'b> {
+			it.next().unwrap().into_group().unwrap()
+		}
+		#[track_caller]
+		fn none<'a, 'b>(it: &mut GroupsIter<'a, 'b>) {
+			assert!(it.next().is_none());
+			// Multiple times as a sanity check
+			assert!(it.next().is_none());
+			assert!(it.next().is_none());
+		}
+
+		let mut cf = parse2(t);
+		{
+			let mut it = cf.iter();
+			let mut it2 = group(&mut it);
+			assert_eq!(string(&mut it2), "pci-drivers");
+			let mut it3 = group(&mut it2);
+			assert_eq!(string(&mut it3), "1af4");
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1000");
+			assert_eq!(string(&mut it4), "drivers/pci/virtio/net");
+			none(&mut it4);
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1001");
+			assert_eq!(string(&mut it4), "drivers/pci/virtio/blk");
+			none(&mut it4);
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1050");
+			assert_eq!(string(&mut it4), "drivers/pci/virtio/gpu");
+			none(&mut it4);
+			none(&mut it3);
+			let mut it3 = group(&mut it2);
+			assert_eq!(string(&mut it3), "8086");
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1616");
+			assert_eq!(string(&mut it4), "drivers/pci/intel/hd graphics");
+			none(&mut it4);
+			none(&mut it3);
+			none(&mut it2);
+			none(&mut it);
+		}
+		assert!(cf.into_error().is_none());
+	}
+
+	#[test]
+	fn partial_iter_group() {
+		let t = br#"(pci-drivers
+	(1af4 ; Red Hat
+		(1000 "drivers/pci/virtio/net")
+		(1001 "drivers/pci/virtio/blk")
+		(1050 "drivers/pci/virtio/gpu"))
+	(8086 ; Intel
+		(1616 "drivers/pci/intel/hd graphics"))) ; intentional space"#;
+		#[track_caller]
+		fn string<'a, 'b>(it: &mut GroupsIter<'a, 'b>) -> &'a str {
+			it.next().unwrap().into_str().unwrap()
+		}
+		#[track_caller]
+		fn group<'a, 'b>(it: &mut GroupsIter<'a, 'b>) -> GroupsIter<'a, 'b> {
+			it.next().unwrap().into_group().unwrap()
+		}
+		#[track_caller]
+		fn none<'a, 'b>(it: &mut GroupsIter<'a, 'b>) {
+			assert!(it.next().is_none());
+			// Multiple times as a sanity check
+			assert!(it.next().is_none());
+			assert!(it.next().is_none());
+		}
+
+		let mut cf = parse2(t);
+		{
+			let mut it = cf.iter();
+			let mut it2 = group(&mut it);
+			assert_eq!(string(&mut it2), "pci-drivers");
+			let mut it3 = group(&mut it2);
+			assert_eq!(string(&mut it3), "1af4");
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1000");
+			assert_eq!(string(&mut it4), "drivers/pci/virtio/net");
+			none(&mut it4);
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1001");
+			drop(it4);
+			drop(it3);
+			let mut it3 = group(&mut it2);
+			assert_eq!(string(&mut it3), "8086");
+			let mut it4 = group(&mut it3);
+			assert_eq!(string(&mut it4), "1616");
+			assert_eq!(string(&mut it4), "drivers/pci/intel/hd graphics");
+			none(&mut it4);
+			none(&mut it3);
+			none(&mut it2);
+			none(&mut it);
+		}
+		assert!(cf.into_error().is_none());
 	}
 }
